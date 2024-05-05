2dc16bd5984883bb143287e8e9b1331ff941c1f6
BMVG-dev <brunoPydev@gmail.com>
2024-05-05 19:43:02 +0100

    urls.py config

--- setup/urls.py
+++ setup/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from recipes.views import home, contact, about
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contact/', contact),
-    path('about/', about),
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
+
 ]
