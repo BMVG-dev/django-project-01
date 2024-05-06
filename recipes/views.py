91bddf6202b97f0dcddbb42f3e2c9c24133f286d
BMVG-dev <brunoPydev@gmail.com>
2024-05-06 21:23:50 +0100

    views.py e home.html

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, './recipes/home.html')
+    return render(request, './recipes/home.html', context={
+        'name': 'BrunoPy dev'
+    })
 
 
 def contact(request):
